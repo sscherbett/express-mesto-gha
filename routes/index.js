131e18fd244ecf495195af0c4938de8eabb37c6a
sscherbett <sch3rbett@yandex.ru>
2023-07-17 00:54:08 +0300

    first try

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const routes = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { ERROR_CODE_NOT_FOUND } = require('../utils/constants');
 
 routes.use('/users', usersRouter);
 routes.use('/cards', cardsRouter);
+routes.use('/*', (req, res) => {
+  res.status(ERROR_CODE_NOT_FOUND).send({
+    message: 'Страница не найдена',
+  });
+});
 
 module.exports = routes;
