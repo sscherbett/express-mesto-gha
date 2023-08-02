e6834afc1e03ade8db478b9657821519352c8c3e
sscherbett <sch3rbett@yandex.ru>
2023-08-02 13:29:59 +0300

    fix: second try

--- controllers/cards.js
+++ controllers/cards.js
@@ -39,7 +39,7 @@ const deleteCard = async (req, res, next) => {
     }
 
     if (req.user._id === card.owner.toString()) {
-      await Card.deleteOne({ _id: req.params.cardId });
+      await Card.deleteOne(card);
       res.status(ERROR_CODE_OK).send({
         message: 'Карточка удалена',
       });
